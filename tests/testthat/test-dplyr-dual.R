c0e9540ebaaa176cf4598b0c0c765cd77145a1b9
Hadley Wickham <h.wickham@gmail.com>
2021-01-24 16:15:04 -0600

    Complete tests for dplyr-dual

--- tests/testthat/test-dplyr-dual.R
+++ tests/testthat/test-dplyr-dual.R
@@ -12,29 +12,21 @@ test_that("joins match local results", {
 
   # primarily testing that basic infrastructure works and that
   # I haven't accidentally typed the wrong verb name somewhere
-  expect_equal(
-    pf1 %>% inner_join(pf2, by = "x") %>% pull(x),
-    c(1)
-  )
-  expect_equal(
-    pf1 %>% left_join(pf2, by = "x") %>% pull(x),
-    c(1, 2)
-  )
-  expect_equal(
-    pf1 %>% right_join(pf2, by = "x") %>% pull(x),
-    c(1, 3)
-  )
-  expect_equal(
-    pf1 %>% full_join(pf2, by = "x") %>% pull(x),
-    c(1, 2, 3)
-  )
+  expect_equal(pf1 %>% inner_join(pf2, by = "x") %>% pull(x), 1)
+  expect_equal(pf1 %>% left_join(pf2, by = "x") %>% pull(x), c(1, 2))
+  expect_equal(pf1 %>% right_join(pf2, by = "x") %>% pull(x), c(1, 3))
+  expect_equal(pf1 %>% full_join(pf2, by = "x") %>% pull(x), c(1, 2, 3))
 
-  expect_equal(
-    pf1 %>% semi_join(pf2, by = "x") %>% pull(x),
-    c(1)
-  )
-  expect_equal(
-    pf1 %>% anti_join(pf2, by = "x") %>% pull(x),
-    c(2)
-  )
+  expect_equal(pf1 %>% semi_join(pf2, by = "x") %>% pull(x), 1)
+  expect_equal(pf1 %>% anti_join(pf2, by = "x") %>% pull(x), 2)
+})
+
+test_that("set operations match local results", {
+  pf1 <- partition(tibble(x = c(1, 2)), default_cluster())
+  pf2 <- partition(tibble(x = c(1, 3)), default_cluster())
+
+  expect_equal(pf1 %>% intersect(pf2) %>% pull(), 1)
+  expect_setequal(pf1 %>% union(pf2) %>% pull(), c(1, 2, 3))
+  expect_equal(pf1 %>% union_all(pf2) %>% pull() %>% sort(), c(1, 1, 2, 3))
+  expect_equal(pf1 %>% setdiff(pf2) %>% pull(), 3)
 })
